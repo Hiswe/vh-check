f32601aa011c1431978b395ed60730e766ab4364
Hiswe <hiswehalya@gmail.com>
2016-06-06 17:43:28 +0700

    ORIENTATION CHANGE – fix value not updated

--- index.js
+++ index.js
@@ -8,9 +8,8 @@
   }
 }(this, function () {
   'use strict';
-  return function vhCheck(cssVarName) {
-    // configurable CSS var
-    cssVarName = typeof cssVarName === 'string' ? cssVarName : 'vh-offset';
+
+  function testVh() {
     // test with fixed
     var fixedTest = document.createElement('div')
     fixedTest.style.cssText = 'position: fixed; top: 0; bottom: 0;'
@@ -20,17 +19,29 @@
     vhTest.style.cssText = 'position: fixed; top: 0; height: 100vh';
     document.documentElement.insertBefore(vhTest, document.documentElement.firstChild);
     // in iOS vh will be bigger
-    const topBottom = fixedTest.offsetHeight;
-    const vh        = vhTest.offsetHeight;
-    const offset    = vh - topBottom;
+    var topBottom = fixedTest.offsetHeight;
+    var vh        = vhTest.offsetHeight;
+    var offset    = vh - topBottom;
     // clean
     document.documentElement.removeChild(fixedTest);
     document.documentElement.removeChild(vhTest);
+    return offset;
+  }
+
+  return function vhCheck(cssVarName) {
+    // configurable CSS var
+    cssVarName = typeof cssVarName === 'string' ? cssVarName : 'vh-offset';
+    var offset = testVh();
     // usefullness check
     if (!offset) return false;
     var style = document.createElement('style');
     style.innerHTML = ':root { --' + cssVarName + ': ' + offset + 'px; }';
     document.body.insertBefore(style, document.body.firstChild);
-    return true
+    // Listen for orientation changes
+    window.addEventListener('orientationchange', function() {
+      var newOffset = testVh();
+      style.innerHTML = ':root { --' + cssVarName + ': ' + newOffset + 'px; }';
+    }, false);
+    return true;
   }
 }));
