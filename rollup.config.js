d7253c071b2e2f8d0e2da67e5208b5dd67fef274
Hiswe <hiswehalya@gmail.com>
2018-03-27 10:31:07 +0700

    BUILD – uglify script

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,30 @@
+import merge from 'lodash.merge'
+import { rollup } from 'rollup'
+import uglify from 'rollup-plugin-uglify'
+
 const NAME = `vh-check`
 
-export default {
+const baseConfig = {
   input: `src/index.js`,
   output: {
     format: `umd`,
-    file: `${ NAME }.js`,
     name: `${ NAME }`,
   },
 }
+
+const devConfig = merge({}, baseConfig, {
+  output: {
+    file: `${ NAME }.dev.js`,
+  },
+})
+
+const prodConfig = merge({}, baseConfig, {
+  output: {
+    file: `${ NAME }.js`,
+  },
+  plugins: [
+    uglify(),
+  ],
+})
+
+export default [ devConfig, prodConfig ]
