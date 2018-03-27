7ad062edfbf124d886dcb53ea5defe64ce4095ee
Hiswe <hiswehalya@gmail.com>
2018-03-27 10:37:17 +0700

    SOURCE – make it prettier

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@
 
 function testVh() {
   // test with fixed
-  var fixedTest = document.createElement('div')
-  fixedTest.style.cssText = 'position: fixed; top: 0; bottom: 0;'
-  document.documentElement.insertBefore(fixedTest, document.documentElement.firstChild)
+  var fixedTest = document.createElement('div');
+  fixedTest.style.cssText = 'position: fixed; top: 0; bottom: 0;';
+  document.documentElement.insertBefore(
+    fixedTest,
+    document.documentElement.firstChild
+  );
   // test with vh
   var vhTest = document.createElement('div');
   vhTest.style.cssText = 'position: fixed; top: 0; height: 100vh';
-  document.documentElement.insertBefore(vhTest, document.documentElement.firstChild);
+  document.documentElement.insertBefore(
+    vhTest,
+    document.documentElement.firstChild
+  );
   // in iOS vh will be bigger
   var topBottom = fixedTest.offsetHeight;
-  var vh        = vhTest.offsetHeight;
-  var offset    = vh - topBottom;
+  var vh = vhTest.offsetHeight;
+  var offset = vh - topBottom;
   // clean
   document.documentElement.removeChild(fixedTest);
   document.documentElement.removeChild(vhTest);
@@ -27,13 +33,17 @@ export default function vhCheck(cssVarName) {
   // configurable CSS var
   cssVarName = typeof cssVarName === 'string' ? cssVarName : 'vh-offset';
   var offset = testVh();
-  // usefullness check
+  // usefulness check
   if (!offset) return false;
   updateCssVar(cssVarName, offset);
   // Listen for orientation changes
-  window.addEventListener('orientationchange', function() {
-    var newOffset = testVh();
-    updateCssVar(cssVarName, newOffset);
-  }, false);
+  window.addEventListener(
+    'orientationchange',
+    function() {
+      var newOffset = testVh();
+      updateCssVar(cssVarName, newOffset);
+    },
+    false
+  );
   return true;
 }
