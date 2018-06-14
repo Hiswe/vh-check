620c15db5aadc1a612e85770f752ac6ab265373c
Hiswe <hiswehalya@gmail.com>
2018-06-14 12:27:44 +0700

    Updates

---
+++ commonjs-bundle.js
@@ -0,0 +1,75 @@
+(function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+'use strict';
+
+var vhCheck = require('../dist/vh-check.js');
+var test = vhCheck();
+
+var p = document.createElement('p');
+var message = !test ? 'not ' : '' + 'needed.<br/>Gap is: ';
+var value = document.documentElement.style.getPropertyValue('--vh-offset');
+value = value ? value : '0px';
+message = message + value;
+p.innerHTML = message;
+
+document.body.insertBefore(p, document.body.firstChild);
+
+},{"../dist/vh-check.js":2}],2:[function(require,module,exports){
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global.vhCheck = factory());
+}(this, (function () { 'use strict';
+
+  function testVh() {
+    // test with fixed
+    var fixedTest = document.createElement('div');
+    fixedTest.style.cssText = 'position: fixed; top: 0; bottom: 0;';
+    document.documentElement.insertBefore(
+      fixedTest,
+      document.documentElement.firstChild
+    );
+    // test with vh
+    var vhTest = document.createElement('div');
+    vhTest.style.cssText = 'position: fixed; top: 0; height: 100vh';
+    document.documentElement.insertBefore(
+      vhTest,
+      document.documentElement.firstChild
+    );
+    // in iOS vh will be bigger
+    var topBottom = fixedTest.offsetHeight;
+    var vh = vhTest.offsetHeight;
+    var offset = vh - topBottom;
+    // clean
+    document.documentElement.removeChild(fixedTest);
+    document.documentElement.removeChild(vhTest);
+    return offset;
+  }
+
+  function updateCssVar(cssVarName, offset) {
+    document.documentElement.style.setProperty('--' + cssVarName, offset + 'px');
+  }
+
+  function vhCheck(cssVarName) {
+    // configurable CSS var
+    cssVarName = typeof cssVarName === 'string' ? cssVarName : 'vh-offset';
+    var offset = testVh();
+    // usefulness check
+    if (!offset) return false;
+    updateCssVar(cssVarName, offset);
+    // Listen for orientation changes
+    window.addEventListener(
+      'orientationchange',
+      function() {
+        var newOffset = testVh();
+        updateCssVar(cssVarName, newOffset);
+      },
+      false
+    );
+    return true;
+  }
+
+  return vhCheck;
+
+})));
+
+},{}]},{},[1]);
---
+++ esmodule-bundle.js
@@ -0,0 +1,168 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({3:[function(require,module,exports) {
+var define;
+var global = arguments[3];
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+(function (global, factory) {
+  (typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : global.vhCheck = factory();
+})(this, function () {
+  'use strict';
+
+  function testVh() {
+    // test with fixed
+    var fixedTest = document.createElement('div');
+    fixedTest.style.cssText = 'position: fixed; top: 0; bottom: 0;';
+    document.documentElement.insertBefore(fixedTest, document.documentElement.firstChild);
+    // test with vh
+    var vhTest = document.createElement('div');
+    vhTest.style.cssText = 'position: fixed; top: 0; height: 100vh';
+    document.documentElement.insertBefore(vhTest, document.documentElement.firstChild);
+    // in iOS vh will be bigger
+    var topBottom = fixedTest.offsetHeight;
+    var vh = vhTest.offsetHeight;
+    var offset = vh - topBottom;
+    // clean
+    document.documentElement.removeChild(fixedTest);
+    document.documentElement.removeChild(vhTest);
+    return offset;
+  }
+
+  function updateCssVar(cssVarName, offset) {
+    document.documentElement.style.setProperty('--' + cssVarName, offset + 'px');
+  }
+
+  function vhCheck(cssVarName) {
+    // configurable CSS var
+    cssVarName = typeof cssVarName === 'string' ? cssVarName : 'vh-offset';
+    var offset = testVh();
+    // usefulness check
+    if (!offset) return false;
+    updateCssVar(cssVarName, offset);
+    // Listen for orientation changes
+    window.addEventListener('orientationchange', function () {
+      var newOffset = testVh();
+      updateCssVar(cssVarName, newOffset);
+    }, false);
+    return true;
+  }
+
+  return vhCheck;
+});
+},{}],1:[function(require,module,exports) {
+'use strict';
+
+var _vhCheck = require('../dist/vh-check.js');
+
+var _vhCheck2 = _interopRequireDefault(_vhCheck);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var test = (0, _vhCheck2.default)();
+var value = document.documentElement.style.getPropertyValue('--vh-offset');
+
+var p = document.createElement('p');
+var message = (!test ? 'not ' : '') + ' needed.<br/>Gap is: ' + (test ? value : '0px');
+p.innerHTML = message;
+
+document.body.insertBefore(p, document.body.firstChild);
+},{"../dist/vh-check.js":3}]},{},[1], null)
