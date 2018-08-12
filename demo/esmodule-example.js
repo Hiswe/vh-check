079b3e62eda0339d34750364e4bd5206dbb2f839
Hiswe <hiswehalya@gmail.com>
2018-08-12 18:24:52 +0700

    DEMO – refine examples

--- demo/esmodule-example.js
+++ demo/esmodule-example.js
@@ -2,12 +2,8 @@ import vhCheck from '../dist/vh-check.js'
 
 const test = vhCheck()
 const value = document.documentElement.style.getPropertyValue('--vh-offset')
-
-const p = document.createElement(`p`)
 const message = `${!test ? 'not ' : ''} needed.<br/>Gap is: ${
   test ? value : '0px'
 }`
-p.innerHTML = message
-
-var domCheckWrapper = document.querySelector('.js-check')
-domCheckWrapper.appendChild(p)
+const domCheckWrapper = document.querySelector('.js-check')
+domCheckWrapper.innerHTML = message
--- demo/redefine-vh.js
+++ demo/redefine-vh.js
@@ -4,13 +4,8 @@ const test = vhCheck({
   redefineVh: true,
 })
 const value = document.documentElement.style.getPropertyValue('--vh')
-
-const p = document.createElement(`p`)
-p.classList.add('with-vh-unit')
 const message = `${!test ? 'not ' : ''} needed.<br/>1vh is: ${
   test ? value : window.innerHeight * 0.01 + 'px'
 }`
-p.innerHTML = message
-
 const domCheckWrapper = document.querySelector('.js-check')
-domCheckWrapper.appendChild(p)
+domCheckWrapper.innerHTML = message
