23e978c6348949006da2b1ad243a7a0166ea2c0c
Hiswe <hiswehalya@gmail.com>
2018-08-13 18:30:41 +0700

    TEST – add for orientation change

--- src/index.test.js
+++ src/index.test.js
@@ -3,9 +3,13 @@ import sinon from 'sinon'
 
 import vhCheck from './index'
 
+function wait() {
+  return new Promise(resolve => setTimeout(resolve, 100))
+}
+
 const CUSTOM_CSS_VAR_NAME = `foo`
 
-test(`default behavior – needed`, t => {
+test.serial(`default behavior – needed`, t => {
   const spy = sinon.spy(document.documentElement.style, 'setProperty')
   const result = vhCheck()
   t.true(result.isNeeded)
@@ -14,18 +18,21 @@ test(`default behavior – needed`, t => {
   document.documentElement.style.setProperty.restore()
 })
 
-test(`default behavior – not needed`, t => {
+test.serial(`default behavior – not needed`, t => {
   const initialWindowHeight = window.innerHeight
   window.innerHeight = 0
   const spy = sinon.spy(document.documentElement.style, 'setProperty')
   const result = vhCheck()
   t.false(result.isNeeded)
-  t.true(spy.notCalled, `set the CSS custom var`)
+  t.true(spy.notCalled, `don't set CSS custom var`)
+  // window.dispatchEvent(new Event('orientationchange'))
+  // await wait()
+  // t.true(spy.notCalled, `don't react on orientation change`)
   document.documentElement.style.setProperty.restore()
   window.innerHeight = initialWindowHeight
 })
 
-test(`force test when not needed`, t => {
+test.serial(`force test when not needed`, t => {
   const initialWindowHeight = window.innerHeight
   window.innerHeight = 0
   const spy = sinon.spy(document.documentElement.style, 'setProperty')
@@ -36,7 +43,7 @@ test(`force test when not needed`, t => {
   window.innerHeight = initialWindowHeight
 })
 
-test(`change property name`, t => {
+test.serial(`change property name`, t => {
   var spy = sinon.spy(document.documentElement.style, 'setProperty')
   vhCheck(CUSTOM_CSS_VAR_NAME)
   t.true(spy.calledOnce, `set the CSS custom var`)
@@ -44,7 +51,7 @@ test(`change property name`, t => {
   document.documentElement.style.setProperty.restore()
 })
 
-test(`change property name with options object`, t => {
+test.serial(`change property name with options object`, t => {
   const spy = sinon.spy(document.documentElement.style, 'setProperty')
   vhCheck({
     cssVarName: CUSTOM_CSS_VAR_NAME,
@@ -54,9 +61,19 @@ test(`change property name with options object`, t => {
   document.documentElement.style.setProperty.restore()
 })
 
-test(`change method`, t => {
+test.serial(`change method`, t => {
   const result = vhCheck({
     redefineVh: true,
   })
   t.is(result.value, result.windowHeight * 0.01)
+})
+
+test.serial.failing(`orientation change`, async t => {
+  const spy = sinon.spy(document.documentElement.style, 'setProperty')
+  vhCheck()
+  t.is(spy.callCount, 1, `initialization call`)
+  window.dispatchEvent(new Event('orientationchange'))
+  await wait()
+  t.is(spy.callCount, 2, `called again after orientationchange`)
+  document.documentElement.style.setProperty.restore()
 })
