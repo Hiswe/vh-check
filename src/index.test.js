a305cc7a26589c85e9acea3e948a10b2df67ecff
Hiswe <hiswehalya@gmail.com>
2018-08-15 15:18:20 +0700

    TESTS – clean

--- src/index.test.js
+++ src/index.test.js
@@ -7,62 +7,67 @@ function wait() {
   return new Promise(resolve => setTimeout(resolve, 10))
 }
 
+test.beforeEach(t => {
+  t.context.initialWindowHeight = window.innerHeight
+  t.context.spy = sinon.spy(document.documentElement.style, `setProperty`)
+})
+
+test.afterEach.always(t => {
+  document.documentElement.style.setProperty.restore()
+  window.innerHeight = t.context.initialWindowHeight
+})
+
 const CUSTOM_CSS_VAR_NAME = `foo`
 
 test.serial(`default behavior – needed`, t => {
-  const spy = sinon.spy(document.documentElement.style, 'setProperty')
   const check = vhCheck()
   t.true(check.isNeeded)
-  t.true(spy.calledOnce, `set the CSS custom var`)
-  t.is(spy.getCall(0).args[0], `--vh-offset`, `has the right name`)
+  t.true(t.context.spy.calledOnce, `set the CSS custom var`)
+  t.is(t.context.spy.getCall(0).args[0], `--vh-offset`, `has the right name`)
   check.unbind()
-  document.documentElement.style.setProperty.restore()
 })
 
 test.serial(`default behavior – not needed`, async t => {
-  const initialWindowHeight = window.innerHeight
   window.innerHeight = 0
-  const spy = sinon.spy(document.documentElement.style, 'setProperty')
   const check = vhCheck()
   t.false(check.isNeeded)
-  t.true(spy.notCalled, `don't set CSS custom var`)
+  t.true(t.context.spy.notCalled, `don't set CSS custom var`)
   window.dispatchEvent(new Event('orientationchange'))
   await wait()
-  t.true(spy.notCalled, `don't react on orientation change`)
-  document.documentElement.style.setProperty.restore()
-  window.innerHeight = initialWindowHeight
+  t.true(t.context.spy.notCalled, `don't react on orientation change`)
 })
 
 test.serial(`force test when not needed`, t => {
   const initialWindowHeight = window.innerHeight
   window.innerHeight = 0
-  const spy = sinon.spy(document.documentElement.style, 'setProperty')
   const check = vhCheck({ force: true })
   t.false(check.isNeeded)
-  t.true(spy.calledOnce, `set the CSS custom var`)
+  t.true(t.context.spy.calledOnce, `set the CSS custom var`)
   check.unbind()
-  document.documentElement.style.setProperty.restore()
-  window.innerHeight = initialWindowHeight
 })
 
 test.serial(`change property name`, t => {
-  var spy = sinon.spy(document.documentElement.style, 'setProperty')
   const check = vhCheck(CUSTOM_CSS_VAR_NAME)
-  t.true(spy.calledOnce, `set the CSS custom var`)
-  t.is(spy.getCall(0).args[0], `--${CUSTOM_CSS_VAR_NAME}`, `has the right name`)
+  t.true(t.context.spy.calledOnce, `set the CSS custom var`)
+  t.is(
+    t.context.spy.getCall(0).args[0],
+    `--${CUSTOM_CSS_VAR_NAME}`,
+    `has the right name`
+  )
   check.unbind()
-  document.documentElement.style.setProperty.restore()
 })
 
 test.serial(`change property name with options object`, t => {
-  const spy = sinon.spy(document.documentElement.style, 'setProperty')
   const check = vhCheck({
     cssVarName: CUSTOM_CSS_VAR_NAME,
   })
-  t.true(spy.calledOnce, `set the CSS custom var`)
-  t.is(spy.getCall(0).args[0], `--${CUSTOM_CSS_VAR_NAME}`, `has the right name`)
+  t.true(t.context.spy.calledOnce, `set the CSS custom var`)
+  t.is(
+    t.context.spy.getCall(0).args[0],
+    `--${CUSTOM_CSS_VAR_NAME}`,
+    `has the right name`
+  )
   check.unbind()
-  document.documentElement.style.setProperty.restore()
 })
 
 test.serial(`change method`, t => {
@@ -74,11 +79,9 @@ test.serial(`change method`, t => {
 })
 
 test.serial(`orientation change`, async t => {
-  const spy = sinon.spy(document.documentElement.style, 'setProperty')
   vhCheck()
-  t.is(spy.callCount, 1, `initialization call`)
-  window.dispatchEvent(new Event('orientationchange'))
+  t.is(t.context.spy.callCount, 1, `initialization call`)
+  window.dispatchEvent(new Event(`orientationchange`))
   await wait()
-  t.is(spy.callCount, 2, `called again after orientationchange`)
-  document.documentElement.style.setProperty.restore()
+  t.is(t.context.spy.callCount, 2, `called again after orientationchange`)
 })
