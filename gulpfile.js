acb47e119251bee21ed35a66bc92ab920650b648
Hiswe <hiswehalya@gmail.com>
2018-10-11 12:18:27 +0700

    REPO – rename demo source files

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,37 @@
 
 const gulp = require('gulp')
 const pug = require('gulp-pug')
-const htmlBeautify = require('gulp-html-beautify')
 const args = require('yargs').argv
+const Parcel = require('parcel-bundler')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const through = require('through2')
+const beautify = require('js-beautify')
+
+function onError(err) {
+  if (err.annotated) {
+    console.log(err.annotated)
+  } else if (err.message) {
+    console.log(err.message)
+  } else {
+    console.log(err)
+  }
+  return this.emit('end')
+}
+
+function beautifyHtml(opts = {}) {
+  function modifyFile(file, enc, cb) {
+    if (file.isNull()) return cb(null, file) // pass along
+    if (file.isStream()) {
+      return cb(new Error(`gulp-beautify: Streaming not supported`))
+    }
+    var str = file.contents.toString(`utf8`)
+    file.contents = new Buffer.from(beautify.html(str, opts))
+    cb(null, file)
+  }
+
+  return through.obj(modifyFile)
+}
 
 const isGhRelease = args[`gh-release`] === true
 const DEMO = `dist-demo`
@@ -11,7 +40,7 @@ const DEMO = `dist-demo`
 console.log(`building for ${isGhRelease ? 'GITHUB' : 'local demo'}`)
 const html = () => {
   return gulp
-    .src([`demo/*.pug`, `!demo/_*.pug`])
+    .src([`src-demo/*.pug`, `!src-demo/_*.pug`])
     .pipe(
       pug({
         locals: {
@@ -19,16 +48,45 @@ const html = () => {
         },
       })
     )
-    .pipe(htmlBeautify({ indent_size: 2 }))
+    .pipe(beautifyHtml({ indent_size: 2 }))
     .pipe(gulp.dest(DEMO))
 }
 
 const css = () => {
-  return gulp.src([`demo/*.css`]).pipe(gulp.dest(DEMO))
+  return gulp.src([`src-demo/*.css`]).pipe(gulp.dest(DEMO))
 }
 
-const js = () => {
-  return gulp.src([`demo/ruler.js`]).pipe(gulp.dest(DEMO))
+const jsRuler = () => {
+  return gulp.src([`src-demo/ruler.js`]).pipe(gulp.dest(DEMO))
 }
+
+function jsCommonJs() {
+  return browserify({
+    cache: {},
+    packageCache: {},
+    debug: false,
+    entries: ['./src-demo/commonjs-example.js'],
+  })
+    .bundle()
+    .on('error', onError)
+    .pipe(source('commonjs-bundle.js'))
+    .pipe(gulp.dest(DEMO))
+}
+
+function jsEsModule() {
+  const entryFiles = [
+    `./src-demo/esmodule-example.js`,
+    `./src-demo/redefine-vh.js`,
+  ]
+  const bundler = new Parcel(entryFiles, {
+    outDir: `./${DEMO}`,
+    watch: false,
+    minify: false,
+    sourceMaps: false,
+  })
+  return bundler.bundle()
+}
+
+const js = gulp.parallel(jsRuler, jsEsModule, jsCommonJs)
 
 exports.demo = gulp.parallel(html, css, js)
